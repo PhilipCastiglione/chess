841aa4b67a04549e9f19181554b63d50267b6b04
Philip Castiglione <philipcastiglione@gmail.com>
2015-09-10 22:27:32 +1000

    added basic test move validations

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -12,23 +12,33 @@ describe Chess do
   end
 
   describe "#get_knight_moves" do
-
+    it "returns the correct moves" do
+      expect(@chess.send(:get_knight_moves, @chess.pieces[:white][1], 'w')).to eq([16, 18])
+    end
   end
 
   describe "#get_bishop_moves" do
-
+    it "returns the correct moves" do
+      expect(@chess.send(:get_bishop_moves, @chess.pieces[:white][2], 'w')).to eq([])
+    end
   end
 
   describe "#get_rook_moves" do
-
+    it "returns the correct moves" do
+      expect(@chess.send(:get_rook_moves, @chess.pieces[:white][0], 'w')).to eq([])
+    end
   end
 
   describe "#get_queen_moves" do
-
+    it "returns the correct moves" do
+      expect(@chess.send(:get_queen_moves, @chess.pieces[:white][3], 'w')).to eq([])
+    end
   end
 
   describe "#get_king_moves" do
-
+    it "returns the correct moves" do
+      expect(@chess.send(:get_king_moves, @chess.pieces[:white][4], 'w')).to eq([])
+    end
   end
 end
 
