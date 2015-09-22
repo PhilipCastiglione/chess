3477ac45fa6c59a89f369d18e6567f15e2254497
Philip Castiglione <philipcastiglione@gmail.com>
2015-09-22 23:25:53 +1000

    add to pawn.rb

--- lib/chess.rb
+++ lib/chess.rb
@@ -2,17 +2,22 @@ require_relative 'chess/gosu.rb'
 require_relative 'chess/pawn.rb'
 
 class Game
+  attr_accessor :pieces, :en_passant
+
   def initialize
     @pieces = {white: [], black: []}
     @active_player = nil
     @next_queen = {white: 2, black: 2}
     @checked = {white: false, black: false}
     @castle = {white: [:wR1, :wR2], black: [:bR1, :bR2]}
-    @en_passant_squares = {white: [], black: []}
+    @en_passant = {white: [], black: []}
   end
 end
 
-Game.new
+g = Game.new
 p = Pawn.new(9, :white)
 p p.col
 p p.row
+p p.moves(g)
+p p.attack_moves(g)
+
--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,25 +1,46 @@
 require_relative 'piece.rb'
 
 class Pawn < Piece
-  def pawn_move_square
+  def move
     self.square + ((self.color == :white)? 8 : -8)
   end
 
-  def pawn_double_move_square
+  def double_move
     self.square + ((self.color == :white)? 16 : -16)
   end
 
-  def west_attack_square
+  def west_attack
     self.square + ((self.color == :white)? 7 : -9)
   end
 
-  def east_attack_square
+  def east_attack
     self.square + ((self.color == :white)? 9 : -7)
   end
 
-  def moves(game_state)
+  def moves(game)
     available_moves = []
+    if unoccupied(move, game.pieces)
+      available_moves << move
+      if unoccupied(double_move, game.pieces)
+        available_moves << double_move
+      end
+    end
+   available_moves
+  end
 
+  def attack_moves(game)
+    available_moves = []
+    if (occupied_enemy(west_attack, game.pieces) ||
+       game.en_passant[self.other_color].include?(west_attack)) &&
+       self.col != 1
+        available_moves << west_attack
+    end
+    if (occupied_enemy(east_attack, game.pieces) ||
+       game.en_passant[self.other_color].include?(east_attack)) &&
+       self.col != 8
+        available_moves << east_attack
+    end
+    available_moves
   end
 end
 
