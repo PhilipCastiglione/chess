f75c7412ba3a32ef5382e9a8dca10ea46061aadf
Philip Castiglione <philipcastiglione@gmail.com>
2015-10-18 00:33:36 +1100

    added test board to test suite

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -4,12 +4,42 @@ describe Game do
 
   before :each do
     @game = Game.new
-    @pawn = Pawn.new(36, :white)
-    @game.pieces[:white] << @pawn
-    @knight = Knight.new(41, :white)
-    @game.pieces[:white] << @knight
-    @bishop = Bishop.new(13, :black)
-    @game.pieces[:black] << @bishop
+
+    @pawn_w1 = Pawn.new(15, :white)
+    @pawn_w2 = Pawn.new(30, :white)
+    @pawn_w3 = Pawn.new(50, :white)
+    @rook_w = Rook.new(8, :white)
+    @knight_w = Knight.new(42, :white)
+    @bishop_w = Bishop.new(26, :white)
+    @queen_w = Queen.new(29, :white)
+    @king_w = King.new(12, :white)
+
+    @pawn_b1 = Pawn.new(13, :black)
+    @pawn_b2 = Pawn.new(33, :black)
+    @pawn_b3 = Pawn.new(48, :black)
+    @rook_b = Rook.new(55, :black)
+    @knight_b = Knight.new(21, :black)
+    @bishop_b = Bishop.new(37, :black)
+    @queen_b = Queen.new(34, :black)
+    @king_b = King.new(51, :black)
+
+    @game.pieces[:white] << @pawn_w1
+    @game.pieces[:white] << @pawn_w2
+    @game.pieces[:white] << @pawn_w3
+    @game.pieces[:white] << @knight_w
+    @game.pieces[:white] << @bishop_w
+    @game.pieces[:white] << @rook_w
+    @game.pieces[:white] << @queen_w
+    @game.pieces[:white] << @king_w
+
+    @game.pieces[:black] << @pawn_b1
+    @game.pieces[:black] << @pawn_b2
+    @game.pieces[:black] << @pawn_b3
+    @game.pieces[:black] << @knight_b
+    @game.pieces[:black] << @bishop_b
+    @game.pieces[:black] << @rook_b
+    @game.pieces[:black] << @queen_b
+    @game.pieces[:black] << @king_b
   end
 
   context Pawn do
