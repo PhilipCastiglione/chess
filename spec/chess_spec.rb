4f6b99731538cf25dde9e8cd0cac1ffc8ea81e0a
Philip Castiglione <philipcastiglione@gmail.com>
2015-10-18 01:18:57 +1100

    added moves to test, all passing except rook

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -45,22 +45,22 @@ describe Game do
   context Pawn do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@pawn_w1.moves(@game)).to eq([23, 31])
-        expect(@pawn_w2.moves(@game)).to eq([38])
-        expect(@pawn_w3.moves(@game)).to eq([58])
-        expect(@pawn_b1.moves(@game)).to eq([5])
-        expect(@pawn_b2.moves(@game)).to eq([25])
-        expect(@pawn_b3.moves(@game)).to eq([40, 32])
+        expect(@pawn_w1.moves(@game).sort).to eq([23, 31].sort)
+        expect(@pawn_w2.moves(@game).sort).to eq([38].sort)
+        expect(@pawn_w3.moves(@game).sort).to eq([58].sort)
+        expect(@pawn_b1.moves(@game).sort).to eq([5].sort)
+        expect(@pawn_b2.moves(@game).sort).to eq([25].sort)
+        expect(@pawn_b3.moves(@game).sort).to eq([40, 32].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@pawn_w1.attack_moves(@game)).to eq([])
-        expect(@pawn_w2.attack_moves(@game)).to eq([37])
-        expect(@pawn_w3.attack_moves(@game)).to eq([])
-        expect(@pawn_b1.attack_moves(@game)).to eq([26])
-        expect(@pawn_b2.attack_moves(@game)).to eq([])
-        expect(@pawn_b3.attack_moves(@game)).to eq([])
+        expect(@pawn_w1.attack_moves(@game).sort).to eq([].sort)
+        expect(@pawn_w2.attack_moves(@game).sort).to eq([37].sort)
+        expect(@pawn_w3.attack_moves(@game).sort).to eq([].sort)
+        expect(@pawn_b1.attack_moves(@game).sort).to eq([].sort)
+        expect(@pawn_b2.attack_moves(@game).sort).to eq([26].sort)
+        expect(@pawn_b3.attack_moves(@game).sort).to eq([].sort)
       end
     end
   end
@@ -68,14 +68,14 @@ describe Game do
   context Rook do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@rook_w.moves(@game)).to eq([0, 9, 10, 11, 16, 24, 32, 40])
-        expect(@rook_b.moves(@game)).to eq([63, 52, 53, 54, 47, 39, 31, 23])
+        expect(@rook_w.moves(@game).sort).to eq([0, 9, 10, 11, 16, 24, 32, 40].sort)
+        expect(@rook_b.moves(@game).sort).to eq([63, 52, 53, 54, 47, 39, 31, 23].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@rook_w.attack_moves(@game)).to eq([48])
-        expect(@rook_b.attack_moves(@game)).to eq([15])
+        expect(@rook_w.attack_moves(@game).sort).to eq([48].sort)
+        expect(@rook_b.attack_moves(@game).sort).to eq([15].sort)
       end
     end
   end
@@ -83,14 +83,14 @@ describe Game do
   context Knight do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@knight_w.moves(@game)).to eq([32, 25, 27, 36, 52, 59, 57])
-        expect(@knight_b.moves(@game)).to eq([36, 27, 11, 4, 6, 31, 38])
+        expect(@knight_w.moves(@game).sort).to eq([32, 25, 27, 36, 52, 59, 57].sort)
+        expect(@knight_b.moves(@game).sort).to eq([36, 27, 11, 4, 6, 31, 38].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@knight_w.attack_moves(@game)).to eq([48])
-        expect(@knight_b.attack_moves(@game)).to eq([15])
+        expect(@knight_w.attack_moves(@game).sort).to eq([48].sort)
+        expect(@knight_b.attack_moves(@game).sort).to eq([15].sort)
       end
     end
   end
@@ -98,14 +98,14 @@ describe Game do
   context Bishop do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@bishop_w.moves(@game)).to eq([17, 19, 35, 44, 53, 62])
-        expect(@bishop_b.moves(@game)).to eq([44, 46, 28, 19, 10, 1])
+        expect(@bishop_w.moves(@game).sort).to eq([17, 19, 35, 44, 53, 62].sort)
+        expect(@bishop_b.moves(@game).sort).to eq([44, 46, 28, 19, 10, 1].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@bishop_w.attack_moves(@game)).to eq([33])
-        expect(@bishop_b.attack_moves(@game)).to eq([30])
+        expect(@bishop_w.attack_moves(@game).sort).to eq([33].sort)
+        expect(@bishop_b.attack_moves(@game).sort).to eq([30].sort)
       end
     end
   end
@@ -113,14 +113,14 @@ describe Game do
   context Queen do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@queen_w.moves(@game)).to eq([22, 38, 47, 20, 11, 2, 28, 27, 36, 43])
-        expect(@queen_b.moves(@game)).to eq([35, 36, 27, 20, 43, 52, 61, 41, 25, 16])
+        expect(@queen_w.moves(@game).sort).to eq([22, 38, 47, 20, 11, 2, 28, 27, 36, 43].sort)
+        expect(@queen_b.moves(@game).sort).to eq([35, 36, 27, 20, 43, 52, 61, 41, 25, 16].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@queen_w.attack_moves(@game)).to eq([])
-        expect(@queen_b.attack_moves(@game)).to eq([])
+        expect(@queen_w.attack_moves(@game).sort).to eq([37, 21].sort)
+        expect(@queen_b.attack_moves(@game).sort).to eq([42, 26].sort)
       end
     end
   end
@@ -128,14 +128,14 @@ describe Game do
   context King do
     describe "#moves" do
       it "returns the valid moves" do
-        expect(@king_w.moves(@game)).to eq([3, 4, 5, 11, 19, 20])
-        expect(@king_b.moves(@game)).to eq([58, 59, 60, 52, 44, 43])
+        expect(@king_w.moves(@game).sort).to eq([3, 4, 5, 11, 19, 20].sort)
+        expect(@king_b.moves(@game).sort).to eq([58, 59, 60, 52, 44, 43].sort)
       end
     end
     describe "#attack_moves" do
       it "returns the valid attack moves" do
-        expect(@king_w.attack_moves(@game)).to eq([13, 21])
-        expect(@king_b.attack_moves(@game)).to eq([50, 42])
+        expect(@king_w.attack_moves(@game).sort).to eq([13, 21].sort)
+        expect(@king_b.attack_moves(@game).sort).to eq([50, 42].sort)
       end
     end
   end
